83cf153e1eeed739fedd648255abfdc123cb22f7
Wenwei-Zhou <wenweizhou0@gmail.com>
2025-08-13 00:27:29 +1000

    Meal-Item
    Fetch meal data, created meal item, and map the item in list. Created the currency format for the price.

---
+++ FoodOrderApp/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header.jsx";
+import Meals from "./Components/Meals.jsx";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Meals />
+
+    </>
+  );
+}
+
+export default App;
