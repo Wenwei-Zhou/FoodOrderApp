70c80fe2b3270034fb5b5d43ab4b4a8f4cbd18bb
Wenwei-Zhou <wenweizhou0@gmail.com>
2025-08-19 05:36:01 +1000

    completed form action
    <Form action={}>

--- FoodOrderApp/src/Components/Checkout.jsx
+++ FoodOrderApp/src/Components/Checkout.jsx
@@ -41,24 +41,13 @@ export default function Checkout() {
   function handleFinish() {
     userProgressCtx.hideCheckout();
     cartCtx.clearCart();
-    setTimeout(() => clearData(), 3000);
-    // clearData();
+    clearData();
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    // event.preventDefault()是阻止表单的默认提交行为
-
-    const fd = new FormData(event.target);
-    // 获取Input里面的所有输入值
-
+  async function handleAction(fd) {
     const customerData = Object.fromEntries(fd.entries());
-    // Object.fromEntries 转成普通对象，注意：每个 input 必须有 name 或者id 属性，否则不会包含在 customerData 中
-    // 转成对象 example：{ "street": "street 5" }
 
-    // console.log(customerData);
-
-    sendRequest(
+    await sendRequest(
       JSON.stringify({
         order: {
           items: cartCtx.items,
@@ -66,22 +55,45 @@ export default function Checkout() {
         },
       })
     );
-
-    // 这个一开始的方法，但创建了useHttp.js之后，这个部分就放在了useHttp.js里面了。
-    // fetch("http://localhost:3000/orders", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json", // 告诉后端是以JSON格式提交一些数据
-    //   },
-    //   body: JSON.stringify({
-    //     // bodys是请求主体，也就是要发送的数据。是以json形式发过去
-    //     order: {
-    //       items: cartCtx.items,
-    //       customer: customerData,
-    //     },
-    //   }),
-    // });
   }
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     // event.preventDefault()是阻止表单的默认提交行为
+
+//     const fd = new FormData(event.target);
+//     // 获取Input里面的所有输入值
+
+//     const customerData = Object.fromEntries(fd.entries());
+//     // Object.fromEntries 转成普通对象，注意：每个 input 必须有 name 或者id 属性，否则不会包含在 customerData 中
+//     // 转成对象 example：{ "street": "street 5" }
+
+//     // console.log(customerData);
+
+//     sendRequest(
+//       JSON.stringify({
+//         order: {
+//           items: cartCtx.items,
+//           customer: customerData,
+//         },
+//       })
+//     );
+
+//     // // 这个一开始的方法，但创建了useHttp.js之后，这个部分就放在了useHttp.js里面了。
+//     // fetch("http://localhost:3000/orders", {
+//     //   method: "POST",
+//     //   headers: {
+//     //     "Content-Type": "application/json", // 告诉后端是以JSON格式提交一些数据
+//     //   },
+//     //   body: JSON.stringify({
+//     //     // bodys是请求主体，也就是要发送的数据。是以json形式发过去
+//     //     order: {
+//     //       items: cartCtx.items,
+//     //       customer: customerData,
+//     //     },
+//     //   }),
+//     // });
+//   }
 
   let actions = (
     <>
@@ -96,22 +108,22 @@ export default function Checkout() {
     return (actions = <span>Sending order data...</span>);
   }
 
-//   console.log("return data!!!!!!", data);
-//   console.log("success", success);
-    // if (data && !error) {
-    //   return (
-    //     <Modal
-    //       open={userProgressCtx.progress === "checkout"}
-    //       onClose={handleFinish}
-    //     >
-    //       <h2>Success!</h2>
-    //       <p>Order submitted successfully</p>
-    //       <p className="modal-actions">
-    //         <Button onClick={handleFinish}>OK</Button>
-    //       </p>
-    //     </Modal>
-    //   );
-    // }
+  //   console.log("return data!!!!!!", data);
+  //   console.log("success", success);
+  // if (data && !error) {
+  //   return (
+  //     <Modal
+  //       open={userProgressCtx.progress === "checkout"}
+  //       onClose={handleFinish}
+  //     >
+  //       <h2>Success!</h2>
+  //       <p>Order submitted successfully</p>
+  //       <p className="modal-actions">
+  //         <Button onClick={handleFinish}>OK</Button>
+  //       </p>
+  //     </Modal>
+  //   );
+  // }
   // 这是网课里面的写法，但不工作。因为当执行Modal时，onClose会触发handleFinish，清空cartCtx.items，导致data变成空数组，所以success变成false。
   // 所以Modal不工作
 
@@ -120,14 +132,18 @@ export default function Checkout() {
       <Modal open={true} onClose={handleFinish}>
         <h2>Success!</h2>
         <p>Order submitted successfully</p>
+        <p className="modal-actions">
+          <Button onClick={handleFinish}>OK</Button>
+        </p>
       </Modal>
     );
   }
-  // 这是自己的写法，不要button了，因为onClose已经触发了handleFinish，所以不需要button了。handlefinish的clearDate()会在3秒后执行，清空data和success变成false，然后Modal会停止工作（不再显示）。
+  // (onSubmit写法)这个写法，不要button了，因为onClose已经触发了handleFinish，所以不需要button了。handlefinish的clearDate()会在3秒后执行，清空data和success变成false，然后Modal会停止工作（不再显示）。
+  // (action法)这种写法是 React Router 的 Form Action 风格（或者 React 18 的 form action API），它确实会在 submit 的时候调用 handleAction，但是它不会自动帮你关闭 Modal。action写法要设置button onClick，在用户点击 OK 或 Modal 关闭的时候，重置 success。
 
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form onSubmit={handleSubmit}>
+      <form action={handleAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         {/* <Input id=/> id是传到后端时的数据名字。example："name": "ww" */}
